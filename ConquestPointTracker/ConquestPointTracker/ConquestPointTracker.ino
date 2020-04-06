9a01d52501032818e3fc7e2b9e21f29fb9cfea94
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-04-06 19:22:53 +0200

    Removed unused code

--- ConquestPointTracker/ConquestPointTracker/ConquestPointTracker.ino
+++ ConquestPointTracker/ConquestPointTracker/ConquestPointTracker.ino
@@ -1,69 +1,27 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-const int switchPin = 6;
-int switchState = 0;
-int prevSwitchState = 0;
-int reply;
+int pointCap = 250;
+
+int team1Points = 0;
+int team2Points = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(16, 2); //size of the lcd (x, y)
-  pinMode(switchPin, INPUT);
-
+  
   lcd.print("Ask the");
   lcd.setCursor(0, 1);
   lcd.print("Crystal Ball!");
+
+  lcd.setCursor(5,1);
+  lcd.print("123");
+
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  switchState = digitalRead(switchPin);
 
-  if(switchState != prevSwitchState){
-    if(switchState == LOW){
-      reply = random(8);
-
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("The ball says:");
-      lcd.setCursor(0,1);
-
-      switch(reply){
-        case 0:
-        lcd.print("Yes!");
-        break;
-
-        case 1:
-        lcd.print("Most likely...");
-        break;
-
-        case 2:
-        lcd.print("Certainly!");
-        break;
-
-        case 3:
-        lcd.print("Outlook good");
-        break;
-
-        case 4:
-        lcd.print("Unsure...");
-        break;
-
-        case 5:
-        lcd.print("Ask again");
-        break;
-
-        case 6:
-        lcd.print("Doubtful");
-        break;
-
-        case 7:
-        lcd.print("No");
-        break;
-      }
-    }
-  }
-
-  prevSwitchState = switchState;
 }
