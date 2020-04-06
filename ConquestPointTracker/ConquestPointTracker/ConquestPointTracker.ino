447f57f2eee6a481ae19c6ec3167d6d78189e698
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-04-06 20:13:34 +0200

    Added support for protothreads

--- ConquestPointTracker/ConquestPointTracker/ConquestPointTracker.ino
+++ ConquestPointTracker/ConquestPointTracker/ConquestPointTracker.ino
@@ -1,4 +1,6 @@
 #include <LiquidCrystal.h>
+#include <pt.h>
+
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 int pointCap = 250;
@@ -6,19 +8,17 @@ int pointCap = 250;
 int team1Points = 0;
 int team2Points = 0;
 
+short selectedTeam = 0; // 1 = team 1, 2 = team 2
+
 
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(16, 2); //size of the lcd (x, y)
-  
-  lcd.print("Ask the");
+
+  lcd.print("Team 1");
   lcd.setCursor(0, 1);
-  lcd.print("Crystal Ball!");
-
-  lcd.setCursor(5,1);
-  lcd.print("123");
-
-  
+  lcd.print("Team 2");
+    
 }
 
 void loop() {
